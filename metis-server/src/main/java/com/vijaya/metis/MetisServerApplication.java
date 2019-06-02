84d702fd51d4d72fc29855e2c1e1688e9ff4554b
MyLaptop <MyLaptop@192.168.1.74>
2019-06-02 17:14:58 +0100

    Initial commit

---
+++ metis-server/src/main/java/com/vijaya/metis/MetisServerApplication.java
@@ -0,0 +1,13 @@
+package com.vijaya.metis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetisServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetisServerApplication.class, args);
+	}
+
+}
---
+++ metis-server/src/test/java/com/vijaya/metis/MetisServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vijaya.metis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetisServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
