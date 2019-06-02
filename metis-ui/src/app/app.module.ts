6243040a30f8d4d7d5e2ef169ed7d455d4374947
Vijaya <vijayatidke@gmail.com>
2019-06-02 18:46:17 +0100

    Added footer and sidenav components

--- metis-ui/src/app/app.module.ts
+++ metis-ui/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
